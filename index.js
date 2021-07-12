142f5170ac058639264547358b74f783bd3b401c
Sean Su <susean17@my.yorku.ca>
2021-07-12 11:49:04 -0400

    stuff

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 var conjugateVerb = require("conjugator/lib/conjugateVerb.js");
+
 var comerConjugation = conjugateVerb("comer");
+
+console.log("The first person plural future subjunctive is: " + 
+  comerConjugation.subjunctive.future.plural.first);
