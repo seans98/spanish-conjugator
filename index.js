79751f6f6c24acbd6368c403e7064528fb73718d
Sean Su <susean17@my.yorku.ca>
2021-07-12 13:50:34 -0400

    added json

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var conjugateVerb = require("conjugator/lib/conjugateVerb.js");
+const { response } = require("express");
+const bodyParser = require("body-parser");
 
-var comerConjugation = conjugateVerb("comer");
+const express = require("express");
+const app = express();
 
-console.log("The first person plural future subjunctive is: " + 
-  comerConjugation.subjunctive.future.plural.first);
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function(req,res){
+    var conjugatedVerb = conjugateVerb(req.body.verb);
+    
+    res.send(conjugatedVerb.subjunctive)
+});
+
+app.listen(3000, function(){
+    console.log("server started on port 3000")
+});
+
+
+
