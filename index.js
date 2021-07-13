c1676957b230b84dc606b4ed0e367d2b752b05cf
Sean Su <susean17@my.yorku.ca>
2021-07-13 11:03:42 -0400

    add new conjugate files with ejs

--- index.js
+++ index.js
@@ -5,21 +5,22 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
-app.use(express.urlencoded({extended: true}))
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", function(req,res){
-    res.sendFile(__dirname + "/index.html")
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req,res){
-    var conjugatedVerb = conjugateVerb(req.body.verb);
-    
-    res.send(conjugatedVerb.subjunctive)
+app.post("/", function (req, res) {
+  var conjugatedVerb = conjugateVerb(req.body.verb, {
+    mood: "indicative"
+  });
+//   conjugatedVerb = JSON.stringify(conjugatedVerb);
+//   res.render("conjugator", { kindOfVerb: conjugatedVerb });
+res.send(conjugatedVerb);
 });
 
-app.listen(3000, function(){
-    console.log("server started on port 3000")
+app.listen(3000, function () {
+  console.log("server started on port 3000");
 });
-
-
-
